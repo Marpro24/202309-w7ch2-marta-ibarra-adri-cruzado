b99a65f536b80c3928098078a27d2cba02792659
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-10-24 21:10:06 +0200

    Add App component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+const App = (): React.ReactElement => {
+return (  
+  <div className="app-container">
+    <header className="main-header">
+     <h1 className="main-title"> Star Wars </h1> 
+    </header> 
+  </div>
+     );
+
+
+}
+
+export default App;
